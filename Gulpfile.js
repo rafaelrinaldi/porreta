e561ba33415685438d01cb289d06c6b919e0aa36
Yuri Brunetto <yuribrunetto@hotmail.com.br>
2016-04-28 22:46:38 -0300

    add landing page

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,9 @@
 "use strict";
 
 var gulp = require("gulp");
-var uglify = require("gulp-uglify");
 var browserSync = require("browser-sync").create();
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
-var rename = require("gulp-rename");
 
 gulp.task("serve", ["sass"], function() {
     browserSync.init({
@@ -14,7 +12,6 @@ gulp.task("serve", ["sass"], function() {
 
     gulp.watch("./public/scss/main.scss", ["sass"]);
     gulp.watch("./index.html").on("change", browserSync.reload);
-    gulp.watch("./public/js/core.js", ["js-watch"]);
 });
 
 gulp.task("sass", function() {
@@ -22,18 +19,6 @@ gulp.task("sass", function() {
         .pipe(autoprefixer())
         .pipe(sass({outputStyle: "compressed"}).on("error", sass.logError))
         .pipe(gulp.dest("./public/css"))
-        .pipe(browserSync.stream());
-});
-
-gulp.task("js-watch", ["js"], browserSync.reload);
-
-gulp.task("js", function(){
-    return gulp.src("./public/js/core.js")
-        .pipe(uglify().on("error", function(e){
-            console.log(e);
-        }))
-        .pipe(rename("./public/js/core.min.js"))
-        .pipe(gulp.dest("./"))
         .pipe(browserSync.stream());
 });
 
